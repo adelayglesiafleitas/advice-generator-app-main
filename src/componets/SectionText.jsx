d2ae04e530290d441f9a808ac021de4c428dbf61
Ajandro <adelaygleisafleitas@gmail.com>
2024-04-15 00:23:24 -0400

    ajsute del 2

--- src/componets/SectionText.jsx
+++ src/componets/SectionText.jsx
@@ -1,17 +1,21 @@
-import { useState, useEffect } from "react";
 import dividerMobile from "../images/pattern-divider-mobile.svg";
+import dividerDesktop from "../images/pattern-divider-desktop.svg";
 import icon from "../images/icon-dice.svg";
+import { useState, useEffect } from "react";
 
 const SectionText = () => {
-  const [data, setData] = useState(null);
-  const [id, setId] = useState(null);
+  const [data, setData] = useState();
+  const [id, setId] = useState();
 
-  const fetchAdvice = (x) => {
+  const randNum = Math.random() * 100;
+  console.log(randNum);
+
+  const fetchApi = (x) => {
     fetch(`https://api.adviceslip.com/advice/${x}`)
-      .then((response) => response.json())
-      .then((result) => {
-        setData(result.slip.advice);
-        setId(result.slip.id);
+      .then((resp) => resp.json())
+      .then((data) => {
+        setData(data.slip.advice);
+        setId(data.slip.id);
       })
       .catch((error) => {
         console.error("Hubo un problema con la solicitud fetch:", error);
@@ -19,29 +23,31 @@ const SectionText = () => {
   };
 
   useEffect(() => {
-    const randNum = Math.floor(Math.random() * 100);
-    fetchAdvice(randNum);
+    fetchApi(randNum);
   }, []);
 
   const handleAdvice = () => {
-    const randNum = Math.floor(Math.random() * 100);
-    fetchAdvice(randNum);
+    fetchApi(randNum);
   };
 
+  console.table(data);
+
   return (
-    <article className="w-[342px] bg-DarkGrayishBlue rounded-xl px-[30px] pt-[10px] pb-16 my-20 mx-auto relative flex flex-col">
-      <h2 className="text-NeonGreen uppercase mb-6 tracking-[4px] text-xs md:w-[540px]">
-        Advice #{id}
-      </h2>
-      <p className="text-LightCyan mb-4">{data}</p>
-      <img src={dividerMobile} alt="imgDivider" />
-      <button
-        className="w-[66px] bg-NeonGreen h-[66px] rounded-full grid place-content-center absolute bottom-[-33px] left-[139px] hover:drop-shadow-[0 0 16px hsl(150,100%,60%)]"
-        onClick={handleAdvice}
-      >
-        <img src={icon} alt="icon-button" />
-      </button>
-    </article>
+    <>
+      <article className="w-[342px] bg-DarkGrayishBlue rounded-xl px-[30px] pt-[10px] pb-16 my-20 mx-auto relative flex flex-col">
+        <h2 className="text-NeonGreen uppercase mb-6 tracking-[4px] text-xs">
+          Advice #<span>{id}</span>{" "}
+        </h2>
+        <p className="text-LightCyan mb-4">{data}</p>
+        <img src={dividerMobile} alt="imgDivider" />
+        <button
+          className="w-[66px] bg-NeonGreen h-[66px] rounded-full grid place-content-center absolute bottom-[-33px] left-[139px] hover:drop-shadow-[0_0_16px_hsl(150,100%,60%)]"
+          onClick={handleAdvice}
+        >
+          <img src={icon} alt="icon-button" />
+        </button>
+      </article>
+    </>
   );
 };
 
