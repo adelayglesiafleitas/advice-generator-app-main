8a183afb3b334d1521bd9091f67fdb8f7e3c24f9
Ajandro <adelaygleisafleitas@gmail.com>
2024-04-15 00:22:02 -0400

    2do

--- src/componets/SectionText.jsx
+++ src/componets/SectionText.jsx
@@ -1,21 +1,17 @@
-import dividerMobile from "../images/pattern-divider-mobile.svg";
-import dividerDesktop from "../images/pattern-divider-desktop.svg";
-import icon from "../images/icon-dice.svg";
 import { useState, useEffect } from "react";
+import dividerMobile from "../images/pattern-divider-mobile.svg";
+import icon from "../images/icon-dice.svg";
 
 const SectionText = () => {
-  const [data, setData] = useState();
-  const [id, setId] = useState();
+  const [data, setData] = useState(null);
+  const [id, setId] = useState(null);
 
-  const randNum = Math.random() * 100;
-  console.log(randNum);
-
-  const fetchApi = (x) => {
+  const fetchAdvice = (x) => {
     fetch(`https://api.adviceslip.com/advice/${x}`)
-      .then((resp) => resp.json())
-      .then((data) => {
-        setData(data.slip.advice);
-        setId(data.slip.id);
+      .then((response) => response.json())
+      .then((result) => {
+        setData(result.slip.advice);
+        setId(result.slip.id);
       })
       .catch((error) => {
         console.error("Hubo un problema con la solicitud fetch:", error);
@@ -23,31 +19,29 @@ const SectionText = () => {
   };
 
   useEffect(() => {
-    fetchApi(randNum);
+    const randNum = Math.floor(Math.random() * 100);
+    fetchAdvice(randNum);
   }, []);
 
   const handleAdvice = () => {
-    fetchApi(randNum);
+    const randNum = Math.floor(Math.random() * 100);
+    fetchAdvice(randNum);
   };
 
-  console.table(data);
-
   return (
-    <>
-      <article className="w-[342px] bg-DarkGrayishBlue rounded-xl px-[30px] pt-[10px] pb-16 my-20 mx-auto relative flex flex-col">
-        <h2 className="text-NeonGreen uppercase mb-6 tracking-[4px] text-xs">
-          Advice #<span>{id}</span>{" "}
-        </h2>
-        <p className="text-LightCyan mb-4">{data}</p>
-        <img src={dividerMobile} alt="imgDivider" />
-        <button
-          className="w-[66px] bg-NeonGreen h-[66px] rounded-full grid place-content-center absolute bottom-[-33px] left-[139px] hover:drop-shadow-[0_0_16px_hsl(150,100%,60%)]"
-          onClick={handleAdvice}
-        >
-          <img src={icon} alt="icon-button" />
-        </button>
-      </article>
-    </>
+    <article className="w-[342px] bg-DarkGrayishBlue rounded-xl px-[30px] pt-[10px] pb-16 my-20 mx-auto relative flex flex-col">
+      <h2 className="text-NeonGreen uppercase mb-6 tracking-[4px] text-xs md:w-[540px]">
+        Advice #{id}
+      </h2>
+      <p className="text-LightCyan mb-4">{data}</p>
+      <img src={dividerMobile} alt="imgDivider" />
+      <button
+        className="w-[66px] bg-NeonGreen h-[66px] rounded-full grid place-content-center absolute bottom-[-33px] left-[139px] hover:drop-shadow-[0 0 16px hsl(150,100%,60%)]"
+        onClick={handleAdvice}
+      >
+        <img src={icon} alt="icon-button" />
+      </button>
+    </article>
   );
 };
 
